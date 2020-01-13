d5c9b54459376ceb4d05b87dcf2ac22d25397572
Raiana <silvaraiana.rs@gmail.com>
2020-01-13 17:48:12 -0300

    atualizando mudanças

--- Documentos/angular/projetos/app1/src/app/shared/frase.model.ts
+++ Documentos/angular/projetos/app1/src/app/shared/frase.model.ts
@@ -1,8 +1,9 @@
 export class Frase{
+
     public fraseEng:string
     public  frasePtbr:string
 
-    constructor (fraseEng:string, frasePtbr){
+    constructor (fraseEng:string, frasePtbr:string){
         this.fraseEng=fraseEng
         this.frasePtbr=frasePtbr
     }
